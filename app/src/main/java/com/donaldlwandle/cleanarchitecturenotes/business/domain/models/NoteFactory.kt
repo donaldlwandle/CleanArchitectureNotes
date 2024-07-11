3a87d696d292e56339576eefd6dc9aea4d5586da
Donald lwandle <donaldlwandle@gmail.com>
2024-07-11 12:16:24 +0200

    Added the following to the domain layer: Note Factory class which like a factory will contain function for creating a single note and a list of notes to be used for unit testing

--- app/src/main/java/com/donaldlwandle/cleanarchitecturenotes/business/domain/models/NoteFactory.kt
+++ app/src/main/java/com/donaldlwandle/cleanarchitecturenotes/business/domain/models/NoteFactory.kt
@@ -1,5 +1,7 @@
 package com.donaldlwandle.cleanarchitecturenotes.business.domain.models
 
+import com.donaldlwandle.cleanarchitecturenotes.business.domain.utils.DateUtil
+import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,7 +10,43 @@ import javax.inject.Singleton
 class NoteFactory
 
 @Inject
-
 constructor(
+    private val dateUtil: DateUtil
+){
 
-)
+    // Used to create a new note
+    //
+    fun createSingleNote(
+        id:String? = null,
+        title: String,
+        body: String? = null
+    ) : Note{
+        return Note(
+            id=id ?: UUID.randomUUID().toString(),
+            title =title,
+            body = body ?: "" ,
+            created_at = dateUtil.getCurrentDateTimestamp(),
+            updated_at = dateUtil.getCurrentDateTimestamp()
+        )
+    }
+
+    //  Generates list of notes( Dummy notes)
+    // Will be used for testing
+    //input number of notes required
+    fun createNotesList (numNotes: Int) :List<Note>{
+        val list : ArrayList<Note> = ArrayList()
+
+        for(i in 0 until  numNotes){
+            list.add(
+                createSingleNote(
+                    id = null,
+                    title = UUID.randomUUID().toString(),
+                    body = UUID.randomUUID().toString()
+                )
+            )
+        }
+
+        return list
+    }
+
+}
